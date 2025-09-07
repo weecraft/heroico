9e1c89508f5950f57a79932fbfa1241af07c83ae
Nyoman Sunima <nyomansunima@gmail.com>
2025-09-08 02:07:46 +0800

    refactor: adjust env config

--- src/shared/libs/config.ts
+++ src/shared/libs/config.ts
@@ -1,38 +1,27 @@
 interface ENV {
-  public: {
-    APP_HOST: string
-    GOOGLE_VERIFICATION: string
-    CONVEX_URL: string
-    CLERK_PUBLISHABLE_KEY: string
-  }
-  private: {
-    CONVEX_DEPLOYMENT: string
-    CLERK_SECRET_KEY: string
-    CLERK_JWT_ISSUER_DOMAIN: string
-  }
+  CONVEX_DEPLOYMENT: string
+  VITE_CONVEX_URL: string
+  VITE_CLERK_PUBLISHABLE_KEY: string
+  CLERK_SECRET_KEY: string
 }
 
-declare global {
-  interface Window {
-    __ENV__: any
-  }
-}
-
-export function loadEnv(): ENV {
-  const isOnBrowser = typeof window !== "undefined"
-  const env = isOnBrowser ? window.__ENV__ : process.env
+/**
+ * Retrieves environment variables, adapting between server-side (Node.js) and
+ * client-side (browser) environments.
+ *
+ * It checks if `window` is defined to determine if the code is running on the server.
+ * On the server, it uses `process.env`. On the client, it uses `import.meta.env`.
+ *
+ * @returns An object containing environment-specific configurations, particularly for Convex.
+ */
+export function getEnv(): ENV {
+  const isServer = typeof window === "undefined"
+  const env = isServer ? process.env : (import.meta.env as any)
 
   return {
-    public: {
-      APP_HOST: env.APP_HOST,
-      GOOGLE_VERIFICATION: env.GOOGLE_VERIFICATION,
-      CONVEX_URL: env.CONVEX_URL!,
-      CLERK_PUBLISHABLE_KEY: env.CLERK_PUBLISHABLE_KEY!,
-    },
-    private: {
-      CONVEX_DEPLOYMENT: env.CONVEX_DEPLOYMENT,
-      CLERK_SECRET_KEY: env.CLERK_SECRET_KEY!,
-      CLERK_JWT_ISSUER_DOMAIN: env.CLERK_JWT_ISSUER_DOMAIN,
-    },
+    CONVEX_DEPLOYMENT: env.CONVEX_DEPLOYMENT,
+    VITE_CONVEX_URL: env.VITE_CONVEX_URL,
+    VITE_CLERK_PUBLISHABLE_KEY: env.VITE_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: env.CLERK_SECRET_KEY,
   }
 }
