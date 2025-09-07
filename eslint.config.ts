e3ea1deca16e7e451443f99baed16c89bb3a2b53
Nyoman Sunima <nyomansunima@gmail.com>
2025-09-08 02:03:14 +0800

    refactor: update the eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,22 +6,18 @@ import {
   react,
   typescript,
 } from "@antfu/eslint-config"
-import pluginQuery from "@tanstack/eslint-plugin-query"
-import convexPlugin from "@convex-dev/eslint-plugin"
+import reactQuery from "@tanstack/eslint-plugin-query"
 
 export default combine(
-  ignores(["build/**", ".react-router/**"]),
+  ignores([".nitro/**", ".output/**", ".tanstack/**", "**/*.gen.ts"]),
   javascript(),
   imports(),
   typescript(),
   react({
     overrides: {
-      "react-dom/no-dangerously-set-innerhtml": "off",
-      "react-hooks-extra/no-direct-set-state-in-use-effect": "off",
       "react-refresh/only-export-components": "off",
       "react/no-unstable-context-value": "off",
     },
   }),
-  pluginQuery.configs["flat/recommended"],
-  convexPlugin.configs.recommended,
+  reactQuery.configs["flat/recommended"],
 )
