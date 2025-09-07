55a660370013c545c3c841f849b50b6ba9ebb0ed
Nyoman Sunima <nyomansunima@gmail.com>
2025-09-08 02:02:53 +0800

    refactor: update prettier config

---
+++ prettier.config.ts
@@ -0,0 +1,10 @@
+//  @ts-check
+/** @type {import('prettier').Config} */
+const config = {
+  semi: false,
+  singleQuote: false,
+  trailingComma: "all",
+  endOfLine: "auto",
+}
+
+export default config
