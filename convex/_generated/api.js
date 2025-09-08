260b3101730ec0fd674c9430e09ec981c8d6294f
Nyoman Sunima <nyomansunima@gmail.com>
2025-09-09 02:07:21 +0800

    chore: update convex generated code

--- convex/_generated/api.js
+++ convex/_generated/api.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 /**
  * Generated `api` utility.
  *
@@ -8,7 +7,7 @@
  * @module
  */
 
-import { anyApi, componentsGeneric } from "convex/server";
+import { anyApi, componentsGeneric } from "convex/server"
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -18,6 +17,6 @@ import { anyApi, componentsGeneric } from "convex/server";
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-export const api = anyApi;
-export const internal = anyApi;
-export const components = componentsGeneric();
+export const api = anyApi
+export const internal = anyApi
+export const components = componentsGeneric()
